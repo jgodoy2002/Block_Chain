24fe65cafe08ebe24135a9c8ec824878bae43e80
jgodoy2002 <j.godoy02@unitec.edu>
2025-02-27 07:59:52 -0600

    Login navega a pantalla principal

--- blockchain/src/Login.jsx
+++ blockchain/src/Login.jsx
@@ -1,34 +1,36 @@
 import React from "react";
 import './Login.css';
-import {FaUser, FaLock} from 'react-icons/fa';
+import { FaUser, FaLock } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate('/home');
+  };
+
   return (
     <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: 'green'}}>
-        <div className="wrapper">
-
-            <h1>Login</h1>
-
-            <div className="input-box">
-            <input type="text" placeholder="Username" />
-            <FaUser className="icon"/>
-            </div>
-
-            <div className="input-box">
-            <input type="password" placeholder="Password" />
-            <FaLock className="icon"/>
-            
-            </div>
-
-            <button type="submit">Login</button>
-
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: 'green'
+    }}>
+      <div className="wrapper">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Username" />
+          <FaUser className="icon" />
         </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" />
+          <FaLock className="icon" />
+        </div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
 }
+
 export default Login;
